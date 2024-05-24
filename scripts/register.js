948bb3187c039511a460c8b35179b5726c8127b6
brandon-bennington <brandon.bennington@codeimmersives.com>
2024-05-23 22:03:35 -0300

    version 1

--- scripts/register.js
+++ scripts/register.js
@@ -1,32 +1,44 @@
 let jocasHouse ={
-    name:"Joca's House",
-    phone:"666-666-6666",
-    hours:{
-        open:"9:00 am",
-        close:"9:00 pm",
+        name:"Joca's House",
+        phone:"666-666-6666",
+        hours:{
+            open:"9:00 am",
+            close:"9:00 pm",
     },
     pets: [//array
-    {
-        name:"Scooby", 
-        age: 12,
-        gender:"Male",
-        service:"Grooming"
-    },
-    {
-        name:"Scrappy",
-        age:3,
-        gender:"Male",
-        service:"Grooming",
-    }
+        {
+            name:"Scooby", 
+            age: 12,
+            gender:"Male",
+            service:"Grooming"
+        },
+        {
+            name:"Scrappy",
+            age:3,
+            gender:"Male",
+            service:"Grooming",
+        },
+        {
+            name:"Goofy",
+            age:14,
+            gender:"Male",
+            service:"Nails cut",
+        },
     ]
 }
 
 function displayName(){
-    console.log(petSalon.pets[1].name);
+    let names="";
+    //suggestion:use a logo
+    for(let i=0;i<3;i++){
+        names+=`<p> ${jocasHouse.pets[i].name} </p>`;
+        console.log(names)
+    }
+    document.getElementById("petNamaes").innerHTML=names;
 }
 
 function countPets(){
-    console.log(petSalon.pets.length);
+    console.log(jocasHouse.pets.length);
 }
 
 
